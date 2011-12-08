893bbff88b9455e3e27352171967b8d5ea714319
j.c.sackett <j.c.sackett@gmail.com>
2011-12-07 21:06:39 -0500

    Basic function running.

--- src/status.py
+++ src/status.py
@@ -3,16 +3,28 @@
 # No guarantees, you're on your own.
 
 import urllib2
+import sys
+
+from BeautifulSoup import BeautifulSoup
+
 
 RACKSPACE = "http://status.apps.rackspace.com/"
 
+def notify(groups):
+    sys.exit(len(groups))
+
 def main():
-    """Main application loop."""
     request = urllib2.Request(RACKSPACE)
     response = urllib2.urlopen(request)
+
     html = response.read()
 
-    print html
+    soup = BeautifulSoup(html)
+
+    groups = soup.findAll('div', 'row-group')
+    groups = [g for g in groups if not g.find('img', 'status-1')]
+
+    notify(groups)
 
 if __name__ == '__main__':
     main()
